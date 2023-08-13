370a2e88547a0de47801144789f52995e2e6dd8f
Zhe LIU <liuzhehayida@gmail.com>
2023-08-13 11:43:59 +0800

    Add files via upload

---
+++ 08_SmallCompounds/TOP2A/readme.txt
@@ -0,0 +1,1 @@
+/home/grads/zliu39/autodock/software/qvina-master/bin/qvina-w --config config.txt --exhaustiveness 30
---
+++ 08_SmallCompounds/TOP2A/run.sh
@@ -0,0 +1,2 @@
+/home/grads/zliu39/autodock/software/qvina-master/bin/qvina-w --config config.txt --exhaustiveness 30
+
